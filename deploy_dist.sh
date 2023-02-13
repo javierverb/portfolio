32b4519ceaa3bc50a930a9e87f114ea695f8bee6
Javier <javier2mil8@gmail.com>
2023-02-13 20:33:20 -0300

    fixed CNAME

--- deploy_dist.sh
+++ deploy_dist.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 ng build --configuration production --output-path docs
+touch docs/CNAME && echo "javierverb.com" > docs/CNAME
 git add .
 git commit -am 'updated frontend'
 git push origin master
