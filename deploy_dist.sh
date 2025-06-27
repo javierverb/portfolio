ce41cdc78441ec91576ee394ff901c49798559e5
javierverb <javier2mil8@gmail.com>
2025-06-27 13:19:31 -0300

    updated deploy dist

--- deploy_dist.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-ng build --configuration production --output-path docs
-touch docs/CNAME && echo "javierverb.com" > docs/CNAME
-git add .
-git commit -am 'updated frontend'
-git push origin master
